3ff7dc7d3d392912f1ae3940c745520e9e7c5012
Mrudul Shah <mrudulloveschess@gmail.com>
2024-06-26 14:38:35 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,101 @@
+const tictacgrid = document.querySelectorAll('.cell')
+let tictactoearray = []
+let previousturnofx
+let winningoutcomes
+let gameover=false
+
+
+let message = document.getElementById('message')
+const resetbutton = document.getElementById('reset-btn')
+
+function ResetButton(ticgrid,arr){
+  
+  ticgrid.forEach((cell)=>{
+    
+    cell.innerText=""
+    
+    
+    
+  })
+  arr.length=0
+  console.log(arr)
+  
+  
+  
+}
+
+function CheckWinner(arr){
+  let winningoutcomes=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+  for(i=0;i<winningoutcomes.length;i++){
+    const[a,b,c]=winningoutcomes[i]
+    if(arr[a]&&arr[a]===arr[b]&&arr[a]===arr[c]){
+      
+      message.innerText=`${arr[a]} Wins`
+      gameover=true
+      
+      
+      
+      
+     
+    }
+    if(arr.length===9 && !arr.includes(undefined)){
+      message.innerText="Game is Draw"
+    
+      setTimeout(()=>{
+        ResetButton(tictacgrid,tictactoearray)
+        message.innerText=""
+        gameover=false
+      },2000)
+    }
+  }
+}
+
+tictacgrid.forEach((cell, index) => {
+  
+  resetbutton.addEventListener('click', () => {
+    tictactoearray = []
+    cell.innerText = ""
+    gameover=false
+    message.innerText=''
+  })
+  
+  cell.addEventListener('click', function()  {
+    
+    if(cell.innerText!=""||gameover ){
+      return
+    }else{
+     
+    if (!tictactoearray.includes('X') && !tictactoearray.includes('O')) {
+      tictactoearray[index]='X'
+      previousturnofx=true
+      console.log(tictactoearray,index)
+      cell.innerText = "X"
+
+
+
+
+
+    } 
+     else if (previousturnofx) {
+      
+      tictactoearray[index]='O'
+      previousturnofx=false
+      console.log(tictactoearray,index)
+      cell.innerText = "O"
+       CheckWinner(tictactoearray)
+       
+
+    } else {
+       
+      tictactoearray[index]='X'
+      previousturnofx=true
+      console.log(tictactoearray,index)
+      cell.innerText = "X"
+       CheckWinner(tictactoearray)
+
+    }
+     
+    }
+    })
+})
+
